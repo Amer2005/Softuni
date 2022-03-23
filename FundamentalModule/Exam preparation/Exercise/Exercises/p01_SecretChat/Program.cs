3788fa86c117791ef934dbde2104cd835e3279b2
Amer <amerpandzharov@gmail.com>
2022-03-23 21:10:14 +0200

    Exam preparation

---
+++ FundamentalModule/Exam preparation/Exercise/Exercises/p01_SecretChat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p01_SecretChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Reveal")
+            {
+                string[] splittedInput = input.Split(new string[] { ":|:" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string actionType = splittedInput[0];
+
+                if (actionType == "InsertSpace")
+                {
+                    int index = int.Parse(splittedInput[1]);
+
+                    message = message.Insert(index, " ");
+                }
+                else if (actionType == "Reverse")
+                {
+                    string substring = splittedInput[1];
+
+                    int index = message.IndexOf(substring);
+
+                    if (index == -1)
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+
+                    message = message.Remove(index, substring.Length);
+
+                    message = message + new string(substring.ToCharArray().Reverse().ToArray());
+                }
+                else if (actionType == "ChangeAll")
+                {
+                    string substring = splittedInput[1];
+
+                    string replacement = splittedInput[2];
+
+                    message = message.Replace(substring, replacement);
+                }
+
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
---
+++ FundamentalModule/Exam preparation/Exercise/Exercises/p02_FancyBarcodes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace p02_FancyBarcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"@#+[A-Z][[A-Za-z0-9]{4,}[A-Z]@#+");
+
+            int numberOfBarcodes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfBarcodes; i++)
+            {
+                string barcode = Console.ReadLine();
+
+                if(!regex.IsMatch(barcode))
+                {
+                    Console.WriteLine("Invalid barcode");
+
+                    continue;
+                }
+
+                StringBuilder allDigits = new StringBuilder(new string(barcode
+                    .ToCharArray()
+                    .Where(x => char.IsDigit(x))
+                    .ToArray()));
+
+                if(allDigits.Length == 0)
+                {
+                    allDigits = new StringBuilder("00");
+                }
+
+                Console.WriteLine($"Product group: {allDigits}");
+            }
+        }
+    }
+}
